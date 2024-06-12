493db398e5fc365ac0f84884ec4fb1381554fc74
Phi Son <sonpq@mail.wgentech.com>
2024-06-12 11:34:17 +0700

    Update daily

--- class_codeptit.py
+++ class_codeptit.py
@@ -1,6 +1,6 @@
 
 from math import *
-class Point():
+class Point_1:
     def __init__(self,x,y) -> None:
         self.x=x
         self.y=y
@@ -20,7 +20,7 @@ def Decimal(a):
 #         print(p1.distance(p2))
 #         t -= 1
 
-class Rectangle():
+class Rectangle:
     def __init__(self,h,w,c) -> None:
         self.h = int(h)
         self.w = int(w)
@@ -38,9 +38,71 @@ class Rectangle():
         a = a.title()
         return a
 
-arr = input().split() 
-if int(arr[0]) > 0 and int(arr[1]) > 0: 
-    r = Rectangle(int(arr[0]), int(arr[1]), arr[2]) 
-    print("{} {} {}".format(r.perimeter(), r.area(), r.color())) 
-else: 
-    print("INVALID")
+
+class Fraction_1:
+    def __init__(self,num, den) -> None:
+        self.num = int(num)
+        self.den = int(den)
+        pass
+    
+    def simplify(self):
+        gcd0 = gcd(self.num, self.den)
+        self.num = self.num//gcd0
+        self.den = self.den//gcd0
+        return "{}/{}".format(self.num,self.den)
+    
+class Fraction_2:
+    def __init__(self,num, den) -> None:
+        self.num = int(num)
+        self.den = int(den)
+        pass
+    
+    def add_fraction(self,other):
+        den_gcd = gcd(self.den,other.den)
+        den_lcm = (self.den * other.den)// den_gcd
+        self.num = self.num * (den_lcm // self.den)
+        other.num = other.num * (den_lcm // other.den)
+        return Fraction_2(self.num + other.num, den_lcm)
+    
+    def simplify(self):
+        gcd0 = gcd(self.num, self.den)
+        self.num = self.num//gcd0
+        self.den = self.den//gcd0
+        return "{}/{}".format(self.num,self.den)
+    
+class Point_2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def distance(self, other):
+        return ((self.x - other.x)**2 + (self.y - other.y)**2)**0.5
+
+class Triangle_1:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+    
+    def is_valid(self):
+        side_ab = self.a.distance(self.b)
+        side_bc = self.b.distance(self.c)
+        side_ca = self.c.distance(self.a)
+        
+        return max(side_ab, side_bc, side_ca)*2<(side_ab+side_bc+side_ca)
+    
+    def perimeter(self):
+        p = self.a.distance(self.b) + self.b.distance(self.c) + self.c.distance(self.a)
+        return "{:.3f}".format(p) if self.is_valid() else "INVALID"
+    
+a = []
+t = int(input())
+for x in range(t):
+    # a.append([float(i) for i in input().split()])
+    a += [float(i) for i in input().split()]
+    
+i = 0
+for index in range(t):
+    triagle = Triangle_1(Point_2(a[i], a[i+1]), Point_2(a[i+2], a[i+3]), Point_2(a[i+4], a[i+5]))
+    print(triagle.perimeter())
+    i += 6
